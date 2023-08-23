b6bdda928632866e97fbc9d6feb05e3ab0db671d
SmallSoft <vadim--111@yandex.ru>
2023-08-23 21:33:22 +0500

    Add restore-bacpac script

---
+++ scripts/restore-bacpac.cmd
@@ -0,0 +1,1 @@
+SqlPackage /Action:Import /SourceFile:"C:\Distr\Traducir\ru-db.08-21-23_00_00_01.bacpac" /TargetConnectionString:"Data Source=.\SQLEXPRESS;Initial Catalog=Traducir.Ru;Integrated Security=True;TrustServerCertificate=True"
